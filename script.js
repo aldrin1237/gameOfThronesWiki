29a066db3ae8f01a6e84f60b864feaf333a832c8
Aldrin <aamistoso10@gmail.com>
2023-09-05 21:40:29 -0700

    finished and added some styles in characters section

--- script.js
+++ script.js
@@ -3,13 +3,36 @@
 async function onClickChange(id) {
     renderDetails(id)
 }
+const CHARACTERS_PER_PAGE = 10;
+let currentPage = 1;
+let lastPage = 6
 
-async function main() {
+async function main(page = 1) {
+    
     const characters = await fetch('https://thronesapi.com/api/v2/Characters')
     const charactersData = await characters.json()
+    
+    const start = (page - 1) * CHARACTERS_PER_PAGE;
+    const end = start + CHARACTERS_PER_PAGE;
+
+    if (page < 1) {
+        currentPage = 1
+        return null
+    }
+    else if (page > lastPage) {
+        currentPage = lastPage
+        return null
+    }
+
+    const charactersToShow = charactersData.slice(start, end)
+    
     const charactersListElement = document.querySelector('.characters__list')
-    // console.log(charactersData)
-    charactersListElement.innerHTML = charactersData.map((character) => charactersListHTML(character)).join('')
+    charactersListElement.innerHTML = charactersToShow.map((character) => charactersListHTML(character)).join('')
+}
+
+function changePage(direction) {
+    currentPage += direction;
+    main(currentPage);
 }
 
 async function renderDetails(characterId) {
@@ -22,48 +45,26 @@ async function renderDetails(characterId) {
 function charactersListHTML(character) {
     return `
     <div class="character" onclick="onClickChange(${character.id})">
-        <p class="character__para">ID: ${character.id}</p>
-        <p class="character__para">Name: ${character.fullName}</p>
+        <p class="character__para">Name: ${character.fullName} </p>
+        <img class="character__img" src="https://thronesapi.com/assets/images/${character.image}">
     </div>`
 }
 
-
-
 function showCharacterDetail(detail) {
     if (!detail) {
         return `<div class="character__detail">Details not available</div>`;
     }
     return `
     <div class="character__detail">
-    <p class="character__first--name">ID: ${detail.id}</p>
+    <img class="characters__second-half--img" src="https://thronesapi.com/assets/images/${detail.image}">
     <p class="character__first--name">First Name: ${detail.firstName || 'N/A'}</p>
     <p class="character__last--name">Last Name: ${detail.lastName || 'N/A'}</p>
     <p class="character__family">Full Name: ${detail.fullName || 'N/A'}</p>
+    <p class="character__family">Title: ${detail.title}</p>
+    <p class="character__family">Family: ${detail.family}</p>
     </div>
     `
 }
-
-/*
-function charactersListHTML(character) {
-    return `
-    <div class="character" onclick="onClickChange(${character.id})">
-        <p class="character__para">ID: ${character.id}</p>
-        <p class="character__para">Name: ${character.fullName}</p>
-    </div>`;
+if (document.querySelector('.characters__list')) {
+    main(currentPage);
 }
-
-function showCharacterDetail(detail) {
-    if (!detail) {
-        return `<div class="character__detail">Details not available</div>`;
-    }
-
-    return `
-    <div class="character__detail">
-    <p class="character__first--name">First Name: ${detail.firstName || 'N/A'}</p>
-    <p class="character__last--name">Last Name: ${detail.lastName || 'N/A'}</p>
-    <p class="character__family">Full Name: ${detail.fullName || 'N/A'}</p>
-    </div>`;
-}
-*/
-
-main()
